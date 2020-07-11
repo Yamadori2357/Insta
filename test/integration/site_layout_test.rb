da94fe8cf2aab4417b5db782b560fab80c5907ee
Ubuntu <ubuntu@ip-172-31-33-55.ap-northeast-1.compute.internal>
2020-07-11 08:39:47 +0000

    Implement basic login

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -6,6 +6,5 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_template 'clone_pages/top_page'
     assert_select "a[href=?]", root_path
     assert_select "a[href=?]", search_page_path
-    assert_select "a[href=?]", my_page_path
   end
 end
