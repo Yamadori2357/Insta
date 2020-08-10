938fe481ff50c65aeea6daf9ab9037db3ba45707
Ubuntu <ubuntu@ip-172-31-33-55.ap-northeast-1.compute.internal>
2020-08-10 14:20:42 +0000

    b

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -10,5 +10,14 @@ class RelationshipsController < ApplicationController
     end
   end
 
+   def create
+    @user = User.find(params[:followed_id])
+    current_user.follow(@user)
+    respond_to do |format|
+      format.html { redirect_to @user }
+      format.js
+    end
+  end
+
   
 end
