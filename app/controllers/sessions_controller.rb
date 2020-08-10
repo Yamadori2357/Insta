c9310b50d211cb3694e987c4c7d66929825f6ddc
Ubuntu <ubuntu@ip-172-31-33-55.ap-northeast-1.compute.internal>
2020-08-10 14:09:07 +0000

    a

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,14 +6,4 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       log_in user
-      remember user
-      redirect_back_or user
-    else
-      # エラーメッセージを作成する
-      flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
-    end
-  end
-
-  
-end
+      remem
